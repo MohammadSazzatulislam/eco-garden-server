6a3f22ec892b50f42c8324b65fb2377999fe2aaf
MohammadSazzatulIslam <rockstarwds@gmail.com>
2023-01-19 17:28:26 +0600

    update

--- index.js
+++ index.js
@@ -5,21 +5,22 @@ const app = express();
 const Products = require("./fakeData.json");
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json("eco garden api running");
+  res.send("eco garden api running");
 });
 
 app.get("/products", (req, res) => {
   res.send(Products);
 });
 
-app.get("/products/:id", (req, res) => {
+app.get("/product/:id", (req, res) => {
   const id = req.params.id;
-  const result = Products.find(id);
+  const result = Products.find((p) => p.id === id);
   res.send(result);
 });
 
-app.listen(() => {
+app.listen(port,() => {
   console.log(`port is running at ${port}`);
 });

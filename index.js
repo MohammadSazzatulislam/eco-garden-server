0103c30a0852c9cec6e7bad0178d6bf66a7aaadd
MohammadSazzatulIslam <rockstarwds@gmail.com>
2023-01-19 16:30:50 +0600

    add api

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+const port = process.env.PORT || 5000;
 const app = express();
+const Products = require("./fakeData.json");
 
 app.use(cors());
 
-app.get("/products/:id", function (req, res, next) {
-  res.json({ msg: "This is CORS-enabled for all origins!" });
+app.get("/", (req, res) => {
+  res.json("eco garden api running");
 });
 
-app.listen(80, function () {
-  console.log("CORS-enabled web server listening on port 80");
+app.get("/products", (req, res) => {
+  res.send(Products);
+});
+
+app.get("/products/:id", (req, res) => {
+  const id = req.params.id;
+  const result = Products.find(id);
+  res.send(result);
+});
+
+app.listen(() => {
+  console.log(`port is running at ${port}`);
 });
